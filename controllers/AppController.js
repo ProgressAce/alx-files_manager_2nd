6975c25d42675ee57d253d6bb5262edf4491fa9b
ProgressAce <daiyaansallie7@gmail.com>
2024-05-15 10:25:12 +0200

    refactor: move functions directly into exports

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,33 +3,30 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-/**
+module.exports = {
+  /**
  * Checks if the Redis and DB connection is active.
  * @param {import('express').Request} req the received http request
  * @param {import('express').Response} res the sent http response
  */
-function getStatus(req, res) {
-  const redisStatus = redisClient.isAlive();
-  const dbStatus = dbClient.isAlive();
+  getStatus: (req, res) => {
+    const redisStatus = redisClient.isAlive();
+    const dbStatus = dbClient.isAlive();
 
-  return res.status(200).json({ redis: redisStatus, db: dbStatus });
-}
+    return res.status(200).json({ redis: redisStatus, db: dbStatus });
+  },
 
-/**
+  /**
  * Returns the number of users and files in the DB.
  * @param {import('express').Request} req the received http request
  * @param {import('express').Response} res the sent http response
  */
-function getStats(req, res) {
-  (async () => {
-    const nbUsers = await dbClient.nbUsers();
-    const nbFiles = await dbClient.nbFiles();
+  getStats: (req, res) => {
+    (async () => {
+      const nbUsers = await dbClient.nbUsers();
+      const nbFiles = await dbClient.nbFiles();
 
-    return res.status(200).json({ users: nbUsers, files: nbFiles });
-  })();
-}
-
-module.exports = {
-  getStatus,
-  getStats,
+      return res.status(200).json({ users: nbUsers, files: nbFiles });
+    })();
+  },
 };
