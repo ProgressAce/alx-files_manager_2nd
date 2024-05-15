48d1a0810c35f210deb9d113da4a0bde2d86d186
ProgressAce <daiyaansallie7@gmail.com>
2024-05-15 12:23:59 +0200

    refactor: structure endpoint definition in class layout

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,32 +1,33 @@
 // Defines the definition of app endpoints
-
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-module.exports = {
+class AppController {
   /**
- * Checks if the Redis and DB connection is active.
- * @param {import('express').Request} req the received http request
- * @param {import('express').Response} res the sent http response
- */
-  getStatus: (req, res) => {
+   * Checks if the Redis and DB connection is active.
+   * @param {import('express').Request} req the received http request
+   * @param {import('express').Response} res the sent http response
+   */
+  static getStatus(req, res) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
 
     return res.status(200).json({ redis: redisStatus, db: dbStatus });
-  },
+  }
 
   /**
- * Returns the number of users and files in the DB.
- * @param {import('express').Request} req the received http request
- * @param {import('express').Response} res the sent http response
- */
-  getStats: (req, res) => {
+   * Returns the number of users and files in the DB.
+   * @param {import('express').Request} req the received http request
+   * @param {import('express').Response} res the sent http response
+   */
+  static getStats(req, res) {
     (async () => {
       const nbUsers = await dbClient.nbUsers();
       const nbFiles = await dbClient.nbFiles();
 
       return res.status(200).json({ users: nbUsers, files: nbFiles });
     })();
-  },
-};
+  }
+}
+
+module.exports = AppController;
--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,78 +4,77 @@ const uuid = require('uuid');
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
-/**
- * Sign-in the user by generating a new authentication token.
- * @param {import('express').Request} req the received http request
- * @param {import('express').Response} res the sent http response
- */
-async function getConnect(req, res) {
-  const basicAuth = req.headers.authorization || '';
-  let email;
-  let pwd;
+class AuthController {
+  /**
+   * Sign-in the user by generating a new authentication token.
+   * @param {import('express').Request} req the received http request
+   * @param {import('express').Response} res the sent http response
+   */
+  static async getConnect(req, res) {
+    const basicAuth = req.headers.authorization || '';
+    let email;
+    let pwd;
 
-  // Parse the authorization header value, as needed.
-  try {
-    const utf8Str = Buffer.from(basicAuth.split('Basic ')[1], 'base64').toString('utf8');
-    [email, pwd] = utf8Str.split(':');
+    // Parse the authorization header value, as needed.
+    try {
+      const utf8Str = Buffer.from(basicAuth.split('Basic ')[1], 'base64').toString('utf8');
+      [email, pwd] = utf8Str.split(':');
 
-    if (!email || !pwd) {
-      return res.status(400).json({ error: 'Incorrect email/password base64 format' });
+      if (!email || !pwd) {
+        return res.status(400).json({ error: 'Incorrect email/password base64 format' });
+      }
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Server error while signing in. Apologies!' });
     }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Server error while signing in. Apologies!' });
+
+    const hashedPwd = sha1(pwd);
+    // const utf8Str = b64.toString('utf-8');
+    // console.log(utf8Str);
+    const user = await dbClient.findUser('email', email);
+    if (!user || user.password !== hashedPwd) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = user._id;
+    let token;
+    try {
+      token = uuid.v4();
+      const key = `auth_${token}`;
+
+      await redisClient.set(key, userId.toString(), 60 * 60 * 24);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Error while connecting.' });
+    }
+
+    return res.status(200).json({ token });
   }
 
-  const hashedPwd = sha1(pwd);
-  // const utf8Str = b64.toString('utf-8');
-  // console.log(utf8Str);
-  const user = await dbClient.findUser('email', email);
-  if (!user || user.password !== hashedPwd) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  const userId = user._id;
-  let token;
-  try {
-    token = uuid.v4();
+  /**
+   * Sign-out the user based on the authentication token.
+   * @param {import('express').Request} req the received http request
+   * @param {import('express').Response} res the sent http response
+   */
+  static async getDisconnect(req, res) {
+    const token = req.header('X-Token') || '';
     const key = `auth_${token}`;
 
-    await redisClient.set(key, userId.toString(), 60 * 60 * 24);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Error while connecting.' });
-  }
+    try {
+      const userId = await redisClient.get(key);
 
-  return res.status(200).json({ token });
-}
+      if (!userId) {
+        return res.status(401).json({ error: 'Unauthorized ' });
+      }
 
-/**
- * Sign-out the user based on the authentication token.
- * @param {import('express').Request} req the received http request
- * @param {import('express').Response} res the sent http response
- */
-async function getDisconnect(req, res) {
-  const token = req.header('X-Token') || '';
-  const key = `auth_${token}`;
-
-  try {
-    const userId = await redisClient.get(key);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized ' });
+      redisClient.del(key);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Error finding token' });
     }
 
-    redisClient.del(key);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: 'Error finding token' });
+    return res.status(204).end();
   }
-
-  return res.status(204).end();
 }
 
-module.exports = {
-  getConnect,
-  getDisconnect,
-};
+module.exports = AuthController;
