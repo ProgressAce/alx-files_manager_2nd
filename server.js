bbb1fa15e8e9715024ef098894a0de70a19b3e7b
ProgressAce <daiyaansallie7@gmail.com>
2024-05-15 09:34:11 +0200

    refactor: change console message

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || '5000';
 const app = express();
 
 app.use('/', routes);
-app.listen(port, () => console.log(`API running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
